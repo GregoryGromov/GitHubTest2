8545cff9c4cd38ddfc60632f42911479d87c2359
gregorygromov <gregorygromow@icloud.com>
2024-06-22 01:39:04 +0300

    Initial Commit

---
+++ GitHubTest2/GitHubTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTest2App.swift
+//  GitHubTest2
+//
+//  Created by Григорий Громов on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
